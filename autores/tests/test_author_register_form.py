0b5649b9880ebd077a4c895ff0cfa4a9bf3bab2c
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-09-20 22:39:53 -0300

    Extraindo métodos e terminando testes dos forms.

--- autores/tests/test_author_register_form.py
+++ autores/tests/test_author_register_form.py
@@ -1,11 +1,9 @@
-from django.test import TestCase as DjangoTestCase
 from unittest import TestCase
 from autores.forms import RegisterForm
 from parameterized import parameterized
-from django.urls import reverse
+from .test_base_mixin import TestBaseMixin
 
 class RegisterFormUnitTest(TestCase):
-
 
     @parameterized.expand([
         ('first_name','Primeiro Nome'),
@@ -33,7 +31,7 @@ class RegisterFormUnitTest(TestCase):
         form = RegisterForm()
         help_text_test = form[field].field.help_text
         self.assertEqual(help_text_test, help_text)
-    
+
 
     @parameterized.expand([
         ('first_name','Ex.: antonio'),
@@ -48,13 +46,13 @@ class RegisterFormUnitTest(TestCase):
         placeholder_test = form[field].field.widget.attrs['placeholder']
         self.assertEqual(placeholder, placeholder_test)
 
-    
+
     @parameterized.expand([
         ('first_name','O campo de primeiro nome não pode ficar vazio!'),
         ('last_name','O campo de último nome não pode ficar vazio!'),
         ('username','O campo de nome de usuário nome não pode ficar vazio!'),
         ('email','O campo de e-mail não pode ficar vazio!'),
-        ('password','O campo de senha não pdoe ficar vazio!'),
+        ('password','O campo de senha não pode ficar vazio!'),
         ('password_confirmation','O campo de confirmação de senha não pode ficar vazio'),
     ])
     def test_if_fields_error_messages_required_is_correct(self, field, required):
@@ -63,7 +61,7 @@ class RegisterFormUnitTest(TestCase):
         self.assertEqual(required_test, required)
 
 
-class RegisterFormIntegrationTest(DjangoTestCase):
+class RegisterFormIntegrationTest(TestBaseMixin):
 
     def setUp(self) -> None:
         self.form_data = {
@@ -82,67 +80,100 @@ class RegisterFormIntegrationTest(DjangoTestCase):
         ('last_name','O campo de último nome não pode ficar vazio!'),
         ('username','O campo de nome de usuário nome não pode ficar vazio!'),
         ('email','O campo de e-mail não pode ficar vazio!'),
-        ('password','O campo de senha não pdoe ficar vazio!'),
+        ('password','O campo de senha não pode ficar vazio!'),
         ('password_confirmation','O campo de confirmação de senha não pode ficar vazio'),
     ])
     def test_if_fields_required_message_is_being_render(self, field, msg):
-        url = reverse('autores:register_validate')
         self.form_data[field] = ''
-        response = self.client.post(url, data=self.form_data, follow=True)
+        response = self.get_response('register_validate','post',self.form_data)
         self.assertIn(msg, response.content.decode('utf-8'))
         self.assertIn(msg, response.context['form'].errors.get(field,''))
 
-    
+
     @parameterized.expand([
         ('first_name','O campo de primeiro nome não pode ficar vazio!'),
         ('last_name','O campo de último nome não pode ficar vazio!'),
         ('username','O campo de nome de usuário nome não pode ficar vazio!'),
         ('email','O campo de e-mail não pode ficar vazio!'),
-        ('password','O campo de senha não pdoe ficar vazio!'),
+        ('password','O campo de senha não pode ficar vazio!'),
         ('password_confirmation','O campo de confirmação de senha não pode ficar vazio'),
     ])
     def test_if_fields_required_message_is_not_being_render(self, field, msg):
-        url = reverse('autores:register_validate')
-        response = self.client.post(url, data=self.form_data, follow=True)
+        response = self.get_response('register_validate','post',self.form_data)
         self.assertNotIn(msg, response.content.decode('utf-8'))
         self.assertNotIn(msg, response.context['form'].errors.get(field, ''))
 
 
     def test_if_register_view_raise_404_on_get_method(self):
-        url = reverse('autores:register_validate')
-        response = self.client.get(url)
+        response = self.get_response('register_validate')
         self.assertEqual(response.status_code, 404)
 
-    
+
     def test_if_email_field_dont_raise_invalid_error_message_if_unique(self):
-        url = reverse('autores:register_validate')
-        response = self.client.post(url, data=self.form_data, follow=True)
+        response = self.get_response(
+            'register_validate',
+            method='post',
+            data=self.form_data,
+            follow=True
+        )
         msg = 'Endereço de e-mail já cadastrado, insira um válido!'
         self.assertNotIn(msg, response.content.decode('utf-8'))
         self.assertNotIn(msg, response.context['form'].errors.get('email', ''))
 
-
     def test_if_email_field_raise_invalid_error_message_if_not_unique(self):
-        url = reverse('autores:register_validate')
-        self.client.post(url, data=self.form_data, follow=True)
+        # Criando o primeiro usuário
+        response = self.get_response(
+            'register_validate',
+            method='post',
+            data=self.form_data,
+            follow=True
+        )
 
-        response = self.client.post(url, data=self.form_data, follow=True)
+        # Criando um segundo usuário com o mesmo e-mail e alterando o seu nome de usuário
+        self.form_data['username'] = "jhondoealter"
+        response = self.get_response(
+            'register_validate',
+            method='post',
+            data=self.form_data,
+            follow=True
+        )
         msg = 'Endereço de e-mail já cadastrado, insira um válido!'
         self.assertIn(msg, response.content.decode('utf-8'))
         self.assertIn(msg, response.context['form'].errors.get('email', ''))
 
 
     def test_if_username_field_dont_raise_invalid_error_message_if_unique(self):
-        url = reverse('autores:register_validate')
-        response = self.client.post(url , data=self.form_data, follow=True)
+        response = self.get_response(
+            'register_validate',
+            method='post',
+            data=self.form_data,
+            follow=True
+        )
         msg = 'Nome de usuário já cadastro, insira um novo!'
         self.assertNotIn(msg, response.content.decode('utf-8'))
         self.assertNotIn(msg, response.context['form'].errors.get('username', ''))
-    
+
     def test_if_username_field_raise_invalid_error_message_if_not_unique(self):
-        url = reverse('autores:register_validate')
-        self.client.post(url, data=self.form_data, follow=True)
-        response = self.client.post(url , data=self.form_data, follow=True)
+        # Cadastrando um primeiro usuário
+        self.get_response(
+            'register_validate',
+            method='post',
+            data=self.form_data,
+            follow=True
+        )
+
+        # Alterando o e-mail para não levantar erro
+        self.form_data['email'] = 'jhondoealter@email.com'
+
+        # Tentando cadastrar um segundo usuário com o mesmo username
+        response = self.get_response(
+            'register_validate',
+            method='post',
+            data=self.form_data,
+            follow=True
+        )
+
+        # Realizando a asserção
         msg = 'Nome de usuário já cadastro, insira um novo!'
         self.assertIn(msg, response.content.decode('utf-8'))
         self.assertIn(msg, response.context['form'].errors.get('username', ''))
--- autores/tests/test_authors_login_form.py
+++ autores/tests/test_authors_login_form.py
@@ -1,16 +1,133 @@
-from django.test import TestCase as DjangoTestCase
+from .test_base_mixin import TestBaseMixin
 from unittest import TestCase
 from parameterized import parameterized
 from autores.forms import LoginForm
 
 
 class LoginFormUnitTest(TestCase):
-    
+
     @parameterized.expand([
-        ('password',''),
+        ('password','Senha'),
+        ('username', 'Nome de Usuário')
     ])
     def test_if_fields_label_is_correct(self, field, label):
         form = LoginForm()
         label_test = form[field].field.label
         self.assertEqual(label_test, label)
-        
+
+    @parameterized.expand([
+        ('username','Digite o seu nome de usuário'),
+        ('password','Insira a sua senha já cadastrada'),
+    ])
+    def test_if_fields_help_text_is_correct(self, field, help_text):
+        form = LoginForm()
+        help_text_test = form[field].field.help_text
+        self.assertEqual(help_text_test, help_text)
+
+
+    @parameterized.expand([
+        ('username','Ex.: antoniocorreia'),
+        ('password','Insira sua senha'),
+    ])
+    def test_if_fields_placeholder_is_correct(self, field, placeholder):
+        form = LoginForm()
+        placeholder_test = form[field].field.widget.attrs['placeholder']
+        self.assertEqual(placeholder, placeholder_test)
+
+
+    @parameterized.expand([
+        ('username','O campo de nome de usuário não pode ficar vazio!'),
+        ('password','O campo de senha não pode ficar vazio!')
+    ])
+    def test_if_fields_error_messages_required_is_correct(self, field, required):
+        form = LoginForm()
+        required_test = form[field].field.error_messages['required']
+        self.assertEqual(required_test, required)
+
+
+class LoginFormIntegrationTest(TestBaseMixin):
+
+    def setUp(self) -> None:
+        self.form_data_register = {
+            'first_name': 'Jhon',
+            'last_name': 'Doe',
+            'username': 'jhondoe',
+            'email': 'jhondoe@email.com',
+            'password': 'P@as1234',
+            'password_confirmation': 'P@as1234'
+        }
+
+        self.form_data_login = {
+            'username': 'jhondoe',
+            'password': 'P@as1234'
+        }
+        return super().setUp()
+
+    @parameterized.expand([
+        'Nome de Usuário',
+        'Senha'
+    ])
+    def test_if_field_label_is_being_render_on_template(self,label):
+        response = self.get_response('login')
+        content = response.content.decode('utf-8')
+        self.assertIn(label, content)
+
+    @parameterized.expand([
+        'Ex.: antoniocorreia',
+        'Insira sua senha'
+    ])
+    def test_if_field_placeholder_is_being_render_on_template(self, placeholder):
+        response = self.get_response('login')
+        content = response.content.decode('utf-8')
+        self.assertIn(placeholder, content)
+
+    @parameterized.expand([
+        'Digite o seu nome de usuário',
+        'Insira a sua senha já cadastrada'
+    ])
+    def test_if_field_help_text_is_being_render_on_template(self, help_text):
+        response = self.get_response('login')
+        content = response.content.decode('utf-8')
+        self.assertIn(help_text, content)
+
+    def test_if_login_view_raise_404_on_get_method(self):
+        response = self.get_response('login_validate')
+        self.assertEqual(response.status_code, 404)
+
+    def test_of_not_login_of_a_not_registered_user(self):
+        response = self.client.login(**self.form_data_login)
+        self.assertFalse(response)
+
+    def test_login_of_registered_user(self):
+        # Criando um usuário
+        self.get_response('register_validate', 'post', self.form_data_register)
+
+        # Realizando o login do usuário criado
+        response = self.client.login(**self.form_data_login)
+        self.assertTrue(response)
+
+    def test_of_not_login_if_username_is_incorrect(self):
+        # Criando um usuário
+        self.get_response('register_validate', 'post', self.form_data_register)
+
+        # Alterando o nome de usuário
+        self.form_data_login['username'] = 'jhondoealter'
+
+        # Tentar realizar o login do usuário com o nome de usuário errado
+        response = self.client.login(**self.form_data_login)
+        self.assertFalse(response)
+
+    def test_of_not_login_if_password_is_incorrect(self):
+        # Criando um usuário
+        self.get_response('register_validate', 'post', self.form_data_register)
+
+        # Alterando a senha
+        self.form_data_login['password'] = 'Qaz321654'
+
+        # Tentar realizar o login do usuário com a senha errada
+        response = self.client.login(**self.form_data_login)
+        self.assertFalse(response)
+
+
+
+
