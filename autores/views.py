34d782ac1a4c4227ad43e6e4d63f1243ebbf2266
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-09-20 22:41:03 -0300

    Consertando as messages e alterando os templates de views.

--- autores/views.py
+++ autores/views.py
@@ -12,21 +12,21 @@ def register(request):
     register_form_data = request.session.get('register_form_data', None)
     form = RegisterForm(register_form_data)
 
-    return render(request, 'autores/pages/register_view.html', context={
+    return render(request, 'autores/pages/register.html', context={
         'form': form,
         'form_action': reverse("autores:register_validate")
     })
 
 
 def register_validate(request):
-  
+
     if not request.POST:
         raise Http404()
-   
+
     register_form_data = request.POST
     request.session['register_form_data'] = register_form_data
     form = RegisterForm(register_form_data)
-   
+
     if form.is_valid():
         user = form.save(commit=False)
         user.set_password(user.password)
@@ -38,7 +38,7 @@ def register_validate(request):
 
 
 def login_view(request):
-    
+
     form = LoginForm()
 
     return render(request, "autores/pages/login.html", context={
@@ -49,7 +49,7 @@ def login_view(request):
 def login_validate(request):
     if not request.POST:
         raise Http404
-    
+
     form = LoginForm(request.POST)
 
     if form.is_valid():
@@ -61,12 +61,12 @@ def login_validate(request):
         if authenticated_user is not None:
             messages.success(request, "Login realizado com sucesso !")
             login(request, authenticated_user)
-        
+
         else:
-            messages.error("Credenciais não autenticadas!")
+            messages.error(request, "Credenciais não autenticadas!")
 
     else:
-        messages.error("Nome de usuário ou senha inválidos, insira os campos novamente!")
+        messages.error(request, "Nome de usuário ou senha inválidos, insira os campos novamente!")
 
     return redirect('autores:login')
 
