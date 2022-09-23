4f6e27a731c6e9b4d8a7f71ed47245dae0e05b9e
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-09-22 22:08:19 -0300

    Criando as urls para as views de edit, read e delete.

--- autores/urls.py
+++ autores/urls.py
@@ -10,4 +10,21 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('login/validate/', views.login_validate, name='login_validate'),
     path('logout/', views.logout_view, name="logout"),
+    path('dashboard/', views.dashboard, name="dashboard"),
+    path(
+        'dashboard/recipe/create/',
+        views.dashboard_create_recipe,
+        name='dashboard_create_recipe'
+    ),
+    path(
+        'dashboard/recipe/delete/',
+        views.dashboard_delete_recipe,
+        name='dashboard_delete_recipe'
+    ),
+    path(
+        'dashboard/recipe/<int:pk>/edit/',
+        views.dashboard_edit_recipe,
+        name='dashboard_edit_recipe'
+    ),
+
 ]
