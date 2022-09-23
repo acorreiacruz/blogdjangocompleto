8369a0bd1ebc6eafbe566887bc3b18970a4a11ca
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-09-23 09:43:47 -0300

    Consertando o script de confirmação de exclusão.

--- base_static/global/src/index.js
+++ base_static/global/src/index.js
@@ -1,14 +1,17 @@
-const escope = () => {
-    const form = document.querySelector(".form-delete");
-    form.addEventListener('submit', (e) =>{
-        e.preventDefault();
+const forms = document.querySelectorAll(".form-delete");
 
-        const confirmDeletion = confirm("Você tem certeza que quer excluir esta receita ?");
-        
-        if(confirmDeletion){
-            form.submit();
-        }
-    });
+const escope = () => {
+    for(const form of forms){
+        form.addEventListener('submit', (e) =>{
+            e.preventDefault();
+
+            const confirmDeletion = confirm("Você tem certeza que quer excluir esta receita ?");
+
+            if(confirmDeletion){
+                form.submit();
+            }
+        });
+    }
 }
 
 escope();
