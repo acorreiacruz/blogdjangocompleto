34415b6cb3af8f75961d7330a20f5fde8ff0af46
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-09-22 22:06:24 -0300

    Criando o form EditRecipeForm.

--- autores/forms.py
+++ autores/forms.py
@@ -2,16 +2,17 @@ import re
 from django import forms
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
-
+from receitas.models import Receitas
+from collections import defaultdict
+from utils.validators import is_positive_number
 
 
 def strong_password(password):
     regex = re.compile(r'^(?=.*[a-z])(?=.[A-Z])(?=.[0-9]).{8,}$')
     if not regex.match(password):
         raise ValidationError(
-            'Sua senha deve conter no mínimo 8 caracteres ,sendo pelo menos um maiúsculo,\
-             e deve possuir pelo menos um número',
-            code='invalid',
+            'Sua senha deve conter no mínimo 8 caracteres ,sendo pelo menos um maiúsculo, e deve possuir pelo menos um número',
+            code='invalid'
         )
 
 
@@ -90,11 +91,11 @@ class RegisterForm(forms.ModelForm):
             'placeholder': "Insira sua senha",
         }),
         error_messages = {
-            'required': "O campo de senha não pdoe ficar vazio!",
+            'required': "O campo de senha não pode ficar vazio!",
         },
-        validators= [
-            strong_password,
-        ],
+        # validators= [
+        #     strong_password,
+        # ],
     )
 
     password_confirmation = forms.CharField(
@@ -130,7 +131,7 @@ class RegisterForm(forms.ModelForm):
                 'Nome de usuário já cadastro, insira um novo!',
                 code = 'invalid',
             )
-        
+
         return username_data
 
 
@@ -143,7 +144,7 @@ class RegisterForm(forms.ModelForm):
                 "Endereço de e-mail já cadastrado, insira um válido!",
                 code = "invalid",
             )
-        
+
         return email_data
 
 
@@ -165,6 +166,8 @@ class RegisterForm(forms.ModelForm):
                 ),
             })
 
+        return cleaned_data
+
 
 class LoginForm(forms.Form):
 
@@ -174,14 +177,14 @@ class LoginForm(forms.Form):
         label_suffix = ':',
         min_length = 3,
         max_length = 20,
-        help_text = "Digite a senha da sua conta!",
+        help_text = "Digite o seu nome de usuário",
         widget = forms.TextInput(attrs={
             'placeholder': 'Ex.: antoniocorreia',
         }),
         error_messages = {
             'required': "O campo de nome de usuário não pode ficar vazio!",
             'max_length': "O nome de usuário deve ter no máximo 20 caracteres, ensira um com tamanho válido!",
-            'min_length': 'O nome de usuário deve ter no mínimo 3 caracteres, encira um com tamango válido!',
+            'min_length': 'O nome de usuário deve ter no mínimo 3 caracteres, ensira um com tamango válido!',
         },
     )
 
@@ -189,11 +192,109 @@ class LoginForm(forms.Form):
         required = True,
         label = "Senha",
         label_suffix = ":",
-        help_text = "Insira a sua senha de usuário já cadastrada",
+        help_text = "Insira a sua senha já cadastrada",
         widget = forms.PasswordInput(attrs={
-            'placeholder': 'Insira sua senha de usuário',
+            'placeholder': 'Insira sua senha',
         }),
         error_messages= {
             'required': "O campo de senha não pode ficar vazio!",
         }
     )
+
+
+class EditRecipeForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.form_errors = defaultdict(list)
+
+    class Meta:
+        model=Receitas
+        fields= [
+            'title', 'description',
+            'preparation_time', 'preparation_time_unit', 'servings',
+            'servings_unit', 'preparation_step',
+            'description','cover',
+        ]
+
+        widgets = {
+            'preparation_step': forms.Textarea(
+                attrs={
+                    'class': 'span-2'
+                }
+            ),
+            'cover': forms.FileInput(
+                attrs={
+                    'class': 'span-2'
+                }
+            ),
+            'servings_unit': forms.Select(
+                choices=(
+                    ('Pessoas','Pessoas'),
+                    ('Pedaços','Pedaços'),
+                    ('Porções','Porções')
+                )
+            ),
+            'preparation_time_unit': forms.Select(
+                choices=(
+                    ('Minutos','Minutos'),
+                    ('Horas','Horas')
+                )
+            ),
+        }
+
+    def clean_preparation_time(self):
+        preparation_time = self.cleaned_data.get('preparation_time')
+
+        if not is_positive_number(preparation_time):
+            self.form_errors['preparation_time'].append(
+                'O tempo de preparo deve ser um número positivo'
+            )
+
+        return preparation_time
+
+    def clean_servings(self):
+        servings = self.cleaned_data.get('servings')
+
+        if not is_positive_number(servings):
+            self.form_errors['servings'].append(
+                'A porção deve ser um número positivo'
+            )
+
+        return servings
+
+    def clean(self,*args, **kwargs):
+        super_clean = super().clean(*args, **kwargs)
+        title = self.cleaned_data.get('title')
+        description = self.cleaned_data.get('description')
+        preparation_step = self.cleaned_data.get('preparation_step')
+
+        if len(title) < 10:
+            self.form_errors['title'].append(
+                'O título deve conter pelo menos 10 caracteres'
+            )
+
+        if len(description) < 20:
+            self.form_errors['description'].append(
+                'A descrição deve conter pelo menos 20 caracteres'
+            )
+
+        if len(preparation_step) < 100:
+            self.form_errors['preparation_step'].append(
+                'Os passos de preparo deve conter pelo menos 100 caracteres'
+            )
+
+        if description == title:
+            self.form_errors['description'].append(
+                'A descrição não pode ser igual ao título'
+            )
+            self.form_errors['title'].append(
+                'O título não pode ser igual a descrição'
+            )
+
+        if self.form_errors:
+            raise ValidationError(self.form_errors)
+
+        return super_clean
+
+
