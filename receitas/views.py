9f4bac222fdfdaf0327f1deecece66a1be1cc480
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-09-23 11:18:51 -0300

    Criando uma CBV para a FBV search.

--- receitas/views.py
+++ receitas/views.py
@@ -1,5 +1,5 @@
-from multiprocessing import context
 from django.views.generic import ListView, DetailView
+from django.views import View
 from django.http import Http404
 from django.shortcuts import render , get_list_or_404 , get_object_or_404
 from .models import Receitas
@@ -13,7 +13,7 @@ PER_PAGE = int(os.environ.get('PER_PAGE',4))
 class ReceitaListViewBase(ListView):
     model = Receitas
     context_object_name = 'receitas'
-    ordering = '-id'
+    ordering = ['-id']
     template_name = 'receitas/pages/home.html'
 
     def get_queryset(self, *args, **kwargs):
@@ -36,14 +36,12 @@ class ReceitaListViewBase(ListView):
 class ReceitaListViewHome(ReceitaListViewBase):
     model = Receitas
     context_object_name = 'receitas'
-    ordering = '-id'
     template_name = 'receitas/pages/home.html'
 
 
 class ReceitaListViewCategory(ReceitaListViewBase):
     model = Receitas
     context_object_name = 'receitas'
-    ordering = '-id'
     template_name = 'receitas/pages/category.html'
 
     def get_queryset(self, *args, **kwargs):
@@ -83,6 +81,32 @@ class ReceitaDetailViewReceita(DetailView):
         })
         return context
 
+
+class ReceitaViewSearch(View):
+
+    def get(self, request):
+        search_term = request.GET.get('search','').strip()
+
+        if not search_term:
+            raise Http404()
+
+
+        receitas = Receitas.objects.filter(
+            Q(Q(title__icontains=search_term) |
+            Q(description__icontains=search_term)
+            ),
+            is_published=True
+        ).order_by("-id")
+
+        obj,pagination_range = make_pagination(request,receitas,PER_PAGE)
+
+        return render(request,'receitas/pages/search.html',context={
+            'search_title': f'Searching for "{search_term}"',
+            'search_term':search_term,
+            'receitas': obj,
+            'pagination_range': pagination_range,
+            'aditional': f'&search={search_term}',
+        })
 
 def search(request):
 
