e85f1d971984ec74be0f26602967e9bed9ec1adc
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-09-26 16:00:09 -0300

    Cadastrando as urls do app api ao projeto.

--- core/urls.py
+++ core/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('receitas.urls')),
     path('autores/',include('autores.urls')),
+    path('api/', include('api.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
 ]
 
