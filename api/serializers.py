dbc74fda32cb21d3c8e07a01b16221f2c551912d
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-09-28 08:28:19 -0300

    Criando os serializers.

---
+++ api/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from receitas.models import Receitas, Category
+from django.contrib.auth.models import User
+
+
+class CategoriaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['id', 'name']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'first_name', 'last_name', 'username', 'email']
+
+
+class ReceitasSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Receitas
+        fields = [
+            'id', 'title', 'description', 'slug', 'preparation_time',
+            'preparation_time_unit', 'servings', 'servings_unit', 'preparation_step',
+            'preparation_step_is_html', 'is_published', 'category', 'author'
+        ]
+    category = CategoriaSerializer(read_only=True)
+    author = UserSerializer(read_only=True)
