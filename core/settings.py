69741fe689362e694d7ad225dbede8d541ab276b
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-09-30 14:24:24 -0300

    Configurando o CORS da API.

--- core/settings.py
+++ core/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'debug_toolbar',
     'rest_framework',
     'rest_framework_simplejwt',
+    'corsheaders',
     # Meus apps
     'receitas',
     'autores',
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -170,4 +172,9 @@ SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('Bearer',)
 }
 
+
+# CORS configuration
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8080',
+]
 
