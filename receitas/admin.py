e96d0ca5dd8802544ec66e42b791e07ff189f409
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-10-01 12:44:28 -0300

    Configurando o model Tag no admin da Receita.

--- receitas/admin.py
+++ receitas/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from .models import Category, Receitas
 from django.contrib.auth.models import User
+from tag.models import Tag
+from django.contrib.contenttypes.admin import GenericStackedInline
+
+
+class TagInline(GenericStackedInline):
+    model = Tag
+    fields = 'name',
+    extra = 1
+
 
 
 admin.site.unregister(User)
@@ -22,10 +31,13 @@ class CategoryAdmin(admin.ModelAdmin):
 class ReceitasAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'created_at', 'author', 'is_published')
     list_display_links = ('id', 'title')
-    search_fields = 'id', 'title' , 'slug', 'description', 'author'
+    search_fields = 'title' , 'slug', 'description', 'author'
     list_per_page = 10
     list_filter = ('is_published', 'category', 'author')
     list_editable = 'is_published',
     prepopulated_fields = {
         'slug': ('title',)
     }
+    inlines = [
+        TagInline
+    ]
