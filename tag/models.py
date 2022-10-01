8ad8d29f4a9569816fd279305ce68970c906e67a
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-10-01 14:10:12 -0300

    Excluindo a relação genérica com o app tag.

--- tag/models.py
+++ tag/models.py
@@ -2,16 +2,11 @@ from random import SystemRandom
 import string
 from django.utils.text import slugify
 from django.db import models
-from django.contrib.contenttypes.models import ContentType
-from django.contrib.contenttypes.fields import GenericForeignKey
 
 
 class Tag(models.Model):
     name = models.CharField(max_length=150)
     slug = models.SlugField(unique=True)
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-    object_id = models.PositiveBigIntegerField()
-    content_object = GenericForeignKey("content_type", "object_id")
 
     def save(self, *args, **kwargs):
         if not self.slug:
