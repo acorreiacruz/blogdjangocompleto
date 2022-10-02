3a8d20be17d34db07918d54e5856878dcd905926
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-10-02 15:39:56 -0300

    Adicionando o JS do tamplete menu.

--- base_static/global/src/index.js
+++ base_static/global/src/index.js
@@ -1,4 +1,12 @@
 const forms = document.querySelectorAll(".form-delete");
+const buttonShowMenu = document.querySelector('.button-show-menu');
+const buttonCloseMenu = document.querySelector('.button-close-menu');
+const menuContainer = document.querySelector('.menu-container');
+const formLogout = document.querySelector('.logout-form');
+const linksLogoutForm = document.querySelectorAll('.link-logout-form');
+const navLinks = document.querySelectorAll('.menu-nav a');
+const buttonShowMenuVisibleClass = 'button-show-menu-visible';
+const menuHiddenClass = 'menu-hidden';
 
 const escope = () => {
     for(const form of forms){
@@ -14,4 +22,53 @@ const escope = () => {
     }
 }
 
+const closeMenu = () => {
+    buttonShowMenu.classList.add(buttonShowMenuVisibleClass);
+    menuContainer.classList.add(menuHiddenClass);
+}
+
+const showMenu = () => {
+    buttonShowMenu.classList.remove(buttonShowMenuVisibleClass);
+    menuContainer.classList.remove(menuHiddenClass);
+}
+
+const deactivateLinks = () => {
+    for(const link of navLinks){
+        link.style.visibility = 'hidden';
+    }
+}
+
+const activateLinks = () => {
+    for(const link of navLinks){
+        link.style.visibility = 'visible';
+    }
+}
+
+const menu = () =>{
+    document.addEventListener('click',(e) => {
+        const el = e.target;
+        if(el.classList.contains('fa-bars')){
+            showMenu();
+            activateLinks();
+        }
+        if(el.classList.contains('fa-circle-xmark')){
+            closeMenu();
+            deactivateLinks();
+        }
+    });
+}
+
+const logout = () => {
+    for(const link of linksLogoutForm){
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            formLogout.submit();
+        });
+    }
+}
+
+
+deactivateLinks();
 escope();
+menu();
+logout();
