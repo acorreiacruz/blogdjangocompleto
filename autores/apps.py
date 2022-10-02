faa815b273dcbd7bf785e7e97960d1256eb66cf3
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-10-02 16:07:28 -0300

    Criando um signal para o model User.

--- autores/apps.py
+++ autores/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class AutoresConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'autores'
+
+    def ready(self, *args, **kwargs):
+        import autores.signals #noqa
+        return super().ready(*args, **kwargs)
---
+++ autores/signals.py
@@ -0,0 +1,17 @@
+from venv import create
+from django.contrib.auth import get_user_model
+from django.db.models.signals import post_save
+from autores.models import Profile
+from django.dispatch import receiver
+
+
+User = get_user_model()
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, *args, **kwargs):
+    if created:
+        profile = Profile.objects.create(
+            author=instance
+        )
+        
+
