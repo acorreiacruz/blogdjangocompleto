325ce01fae8a924ea25480f3da60ff60a39bcff3
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-10-02 16:06:46 -0300

    Criando um model Profile para os authors.

--- autores/models.py
+++ autores/models.py
@@ -1,3 +1,8 @@
+from email.policy import default
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    bio = models.TextField(default='', blank=True)
+    author = models.OneToOneField(User, on_delete=models.CASCADE)
