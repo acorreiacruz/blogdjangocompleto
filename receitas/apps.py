655e65f1c2429a7a2ffa488dce185ee4c3d00d92
acorreiacruz <antoniocorreiacruz@outlook.com>
2022-10-03 21:00:41 -0300

    Criando signals para o app receitas.

--- receitas/apps.py
+++ receitas/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class ReceitasConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'receitas'
+
+    def ready(self, *args, **kwargs):
+        import receitas.signals # noqa
+        return super().ready(*args, **kwargs)
